da2f2fdbcdd3e432ed0517a4944fba4839ec7f46
Praveen RS <praveen7200429516@gmail.com>
2026-04-09 14:19:37 +0530

    Mock1 completed

---
+++ round2.js
@@ -0,0 +1,20 @@
+var a = 20
+console.log(a)
+
+// let a = 30
+
+// const a = 45
+
+var b = 20
+if (b==20){
+    console.log("true")
+}
+
+function alert(){
+ onclick=alert("you have clicked me")
+}
+
+if(a==b){
+    console.log()
+}
+console.log(a===b)
